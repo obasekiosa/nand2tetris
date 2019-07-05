19348df3806c7e360d934f1b850e7423c2a244ca
Obaseki Osakpolor <obasekiosa@gmail.com>
2019-07-05 22:06:15 +0100

    Implement basic Assembler (L version)

--- projects/06/assembler.py
+++ projects/06/assembler.py
@@ -1,95 +1,71 @@
-import string
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  5 17:02:46 2019
 
+@author: Obaseki Osakpolor
+"""
+import sys
 
-file_name = NULL
-file_extention = NULL
-
-if file_extention != '.asm':
-    exit()
-# open file XXX.asm
-source_file = open(file_name)
-# create  destination file XXX.hack
-destination_file = open(file_name + ".hack", "w+")
-
-# read each line
-line = True
-while line:
-    line = source_file.readline()
-    # process each line
-    instruction_code = ProcessLine(line)
-    # write each line to destination file
-    if instruction_code:
-        destination_file.write(instruction_code)
-
-# close files
-destination_file.close()
-source_file.close()
-# end program
-exit()
-
-
-# process each line
-def ProcessLine(line):
-    new_line = line[:]
-    if new_line:
-        # remove comments
-        new_line = RemoveComments(line)
-        if new_line
-            # remove white space
-            new_line = RemoveWhiteSpace(new_line)
-            if new_line:
-                # parse instructions
-                instruction, instruction_type = ParseInstruction(new_line)
-                if instruction_type == 1 or instruction_type == 0:
-                    # translate instructions
-                    return TranslateInstruction(instruction, instruction_type)
-                else:
-                    return ''
-            else:
-                return ''
+def ConvertToBinary15(number):
+    bin_num = ''
+    while number != 0:
+        if number % 2 == 0:
+            bin_num += '0'
         else:
-            return ''
-    else:
-        return ''
-
-# remove comments
+            bin_num += '1'
+        number = number // 2
+        
+    zero_padding = ''
+    for i in range(15 - len(bin_num)):
+        zero_padding += '0'
+        
+    return zero_padding + bin_num[::-1]
+    
+        
 def RemoveComments(line):
     # search for '//' in input string
-    loc = line.find(line)
-    # remove all strings strating from position of '//'
-    new_line = line[:loc]
+    loc = line.find("//")
+    if loc == -1:
+        new_line = line
+    else:
+        # slice string up to position of '//'
+        new_line = line[:loc]
     # return proccessed string
     return new_line
-
-# remove white space
+    
 def RemoveWhiteSpace(line):
     new_line = ''
     # replace all nonalphanumeric characters with ''
+    nonallowable = " \n\t\r"
     for e in line:
-        if e.isalphanun():
+        if e not in nonallowable:
             new_line += e
     # return proccessed string
     return new_line
 
-# parse instructions
 def ParseInstruction(line):
     # check if strings begins with @
     tokens = line.split('@', 1)
     if tokens[0] == '' and len(tokens) > 1:
         # return (instruction, comandtype)
         return (tokens[1], 0)
-    else
+    else:
         # split string into left and right of '=' (destinaton, computation)
         token_eq = line.split('=', 1)
         # split string into left and right of ';' (computation, jump)
-        token_semicolon = tokens[1].split(';', 1)
+        token_semicolon = token_eq[len(token_eq) - 1].split(';', 1)
         # (destination, computation, jump)
-        tokens = token_eq[0] + token_semicolon
+        if len(token_eq) > 1:
+            tokens = [token_eq[0]] + token_semicolon
+        else:
+            tokens = [''] + token_semicolon
+        if len(token_semicolon) == 1:
+            tokens += ['']
         instruction = tuple(tokens)
         # return (instruction, comandtype)
         return (instruction, 1)
+    
 
-# translate instructions
 def TranslateInstruction(instruction, instruction_type):
     CODE = {
         'comp': { '0':'0101010',
@@ -154,36 +130,123 @@ def TranslateInstruction(instruction, instruction_type):
     # if A-instruction
     if instruction_type == 0:
         # convert numeric value to 15 bit binary
+        instruction = int(instruction)
+        if instruction < 0:
+            print("Invalid command: negative address")
+            exit()
+        else:
+            binary_instruction = ConvertToBinary15(instruction)
+        
+        if len(binary_instruction) > 15:
+            print("Invalid command: address out of range")
+            exit()
         # append '0' to the beginning of 15 bit result
         # return result
-        return '0' + str(bin(int(instruction))
+        return '0' + binary_instruction
 
     # if C-instruction
-    if instruction_type == 1
-    # get computation code
-    CODE['comp'][instruction[1]]
-    # get destination code
-    # get jump code
-    # append all together with '111' at the beginning
-    # return result
-    return '111' + CODE['comp'][instruction[1]] + CODE['dest'][instruction[0]] + CODE['jump'][instruction[2]]
+    if instruction_type == 1:
+        # get computation code
+        try:
+            comp_code = CODE['comp'][instruction[1]]
+        except:
+            print("Invalid command: invalid computation code")
+            exit()
+        # get destination code
+        try:
+            dest_code = CODE['dest'][instruction[0]]
+        except:
+            print("Invalid command: invalid destination code")
+            exit()
+            
+        # get jump code
+        try:
+            jump_code = CODE['jump'][instruction[2]]
+        except:
+            print("Invalid command: invalid jump code")
+            exit()
+        
+        # append all together with '111' at the beginning
+        # return result
+        return '111' + comp_code + dest_code + jump_code
+#
+# process each line
+def ProcessLine(line):
+    new_line = line[:]
+    if new_line:
+        # remove comments
+        new_line = RemoveComments(new_line)
+        if new_line:
+            # remove white space
+            new_line = RemoveWhiteSpace(new_line)
+            if new_line:
+                # parse instructions
+                instruction, instruction_type = ParseInstruction(new_line)
+                if instruction_type == 1 or instruction_type == 0:
+                    # translate instructions
+                    return TranslateInstruction(instruction, instruction_type)
+                else:
+                    return None
+            else:
+                return None
+        else:
+            return None
+    else:
+        return None
 
+if len(sys.argv) != 2:
+    print("1 argument needed")
+    exit()
+    
+if '.' not in sys.argv[1]:
+    print("file extention needed")
+    exit()
+    
+file_full_name = sys.argv[1].split(".", 1)
+file_name = file_full_name[0]
+file_extention = file_full_name[1]
 
-101010
-111111
-111010
-001100
-110000
-001101
-110001
-001111
-110011
-011111
-110111
-001110
-110010
-000010
-010011
-000111
-000000
-010101
+if file_extention.lower() != 'asm':
+    print("non assembly file give, provide a .asm file")
+    exit()
+    
+    
+# open file XXX.asm
+try:
+    source_file = open(file_name + "." + file_extention, "r")
+except:
+    print("No such file in directory.")
+    exit()
+#create  destination file XXX.hack
+try:
+    destination_file = open(file_name + ".hack", "w+")
+except:
+    print("Could not create Destination file")
+    exit()
+
+# read each line 
+read = True
+line = source_file.readline()
+while read:
+    if line:
+        # process each line
+        instruction_code = ProcessLine(line)
+        # write each line to destination file
+        if instruction_code:
+            destination_file.write(instruction_code)
+        line = source_file.readline()
+        if line and instruction_code:
+            destination_file.write('\n')
+    else:
+        read = False
+    
+# close files
+try:
+    destination_file.close()
+    source_file.close()
+except:
+    print("could not close files")
+finally:
+    # end program
+    exit()
+
